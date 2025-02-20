ae3bfd27e0918e23f6a00d09c473e02cee0f12bb
nguyenhuuthanh2301 <nguyenhuuthanh00205@gmail.com>
2025-02-20 15:31:11 +0700

    Add

---
+++ Source/cautruifelse.js
@@ -0,0 +1,9 @@
+// Hiển thị hộp thoại nhập câu trả lời
+let value = prompt('What is the "official" name of JavaScript?');
+
+// Kiểm tra câu trả lời
+if (value === "ECMAScript") {
+    alert("Right!");
+} else {
+    alert("Didn’t know? ECMAScript!");
+}
